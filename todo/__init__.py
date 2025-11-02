7a125639dc8d2c5e8af0df33e8b1026f8e8b34fa
shamar12334 <weekesshamar38@gmail.com>
2025-11-02 08:46:11 -0500

    Setup and configured the application returning a route to a page that returns welcome to the todo app

---
+++ todo/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    #create and config app
+    app = Flask(__name__,instance_relative_config=True)
+    app.config.from_mapping(
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.instance_path,'todo.sqlite'),
+    )
+    if test_config is None:
+        #load the instnce config, if it exisits, when not testing
+        app.config.from_pyfile('config.py',silent=True)
+    else:
+        #load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    #ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    #page that says hello
+    @app.route('/hello')
+    def hello():
+        return'welcome to the todo app'
+
+    return app
