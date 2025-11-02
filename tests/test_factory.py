f02aac679b5de422eb786568766a6492e039439f
shamar12334 <weekesshamar38@gmail.com>
2025-11-02 09:16:15 -0500

    Created a two test files to test the app factory to see if it returns welcome to the todo app

---
+++ tests/test_factory.py
@@ -0,0 +1,10 @@
+from todo import create_app
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+def test_hello(client):
+    response =client.get('/hello')
+    assert response.data == b'welcome to the todo app'
+
