5ab73c4cc920b692f1b3aeef0d2d6289249599f5
shamar12334 <weekesshamar38@gmail.com>
2025-11-03 18:03:47 -0500

    Added the logic for register and login views

--- todo/auth.py
+++ todo/auth.py
@@ -6,4 +6,51 @@ from flask import (
 from werkzeug.security import check_password_hash,generate_password_hash
 from todo.db import get_db
 bp = Blueprint('auth',name,url_prefix='/auth')
+@bp.route('/register', methods=('GET','POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
 
+        if not username:
+            error ='Username is required'
+        elif not password:
+            error ='Password is required'
+
+        if error is None:
+            try:
+                db.execute(
+                        "INSERT INTO user (usernaem,password) VALUES (?,?)",
+                        (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error =f"User {username} is already registerd."
+            else:
+                return redirect(url_for("auth.login"))
+        flash(error)
+    return render_template('auth/register.html')
+@bp.route('/login', methods=('GET','POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user =db.execute(
+                'SELECT * FROM user WHERE username =?',(username,)
+        ).fetchone()
+
+        if user is None:
+            error ='Incorrect username'
+        elif not check_password_hash(user['password'],password):
+            error ='Incorrect password'
+
+        if error is None:
+            session.clear()
+            session['user_id'] =user['id']
+            return redirect(url_for('index'))
+        flask(error)
+    return render_template('auth/login.html')
